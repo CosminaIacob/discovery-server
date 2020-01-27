bb266d06f3f83c62aa7682360b771406d9ba0683
Cosmina Iacob <cosmina.iacob94@gmail.com>
2020-01-27 17:23:16 +0200

    #1 Initial commit - Eureka

---
+++ src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package io.javabrains.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// tells Spring Boot that this is an Eureka server
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
